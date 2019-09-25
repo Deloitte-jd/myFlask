4af61f4b029b2a160ee3f8f45db1f26dae25eee9
Deloitte-jd <55453031+Deloitte-jd@users.noreply.github.com>
2019-09-25 23:37:59 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask,render_template
 
 import pandas as pd
 import numpy as np
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
 
 @app.route('/')
 def guide():
@@ -12,36 +15,36 @@ def guide():
 
 @app.route('/introduction')
 def INTRO():
-    return render_template('introduction.html')
+    return render_template('bootstrap/introduction.html')
 
 @app.route('/page1')
 def PAGE1():
-    return render_template('page1.html')
+    return render_template('bootstrap/page1.html')
 
 @app.route('/page2')
 def PAGE2():
-    return render_template('page2.html')
+    return render_template('bootstrap/page2.html')
 
 @app.route('/page3')
 def PAGE3():
-    return render_template('page3.html')
+    return render_template('bootstrap/page3.html')
 
 @app.route('/page4')
 def PAGE4():
-    return render_template('page4.html')
+    return render_template('bootstrap/page4.html')
 
 @app.route('/page5')
 def PAGE5():
-    return render_template('page5.html')
+    return render_template('bootstrap/page5.html')
 
 @app.route('/page6')
 def PAGE6():
-    return render_template('page6.html')
+    return render_template('bootstrap/page6.html')
 
 @app.route('/page7')
 def PAGE7():
-    return render_template('page7.html')
-
+    return render_template('bootstrap/page7.html')
 
 if __name__ == '__main__':
     app.run()
+
